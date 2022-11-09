5a1636341f750278e89dc0b0959dfcdaad62925e
youssef115 <58666308+youssef115@users.noreply.github.com>
2022-11-09 23:39:31 +0100

    add style to the items in the side bar

--- src/components/youssefComponents/sideMenu/sideMenu.js
+++ src/components/youssefComponents/sideMenu/sideMenu.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Link}from 'react-router-dom'
 import SideMenuItems from "../sideMenuItems/SideMenuItems";
-import {faUserCheck,faUserXmark,faUsers} from '@fortawesome/free-solid-svg-icons'
+import {faUserCheck,faUserXmark,faUsers, faShuttleSpace} from '@fortawesome/free-solid-svg-icons'
 import '../../../App.css'
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
@@ -10,7 +11,6 @@ function SideMenu() {
   const [isActiveEnseignant,setActiveEnseignant]=React.useState(false);
   const [isActiveEtudiant,setActiveEtudiant]=React.useState(false);
 
-  
   return (
   <>
     <div  className={'side-menu'} >
@@ -25,30 +25,79 @@ function SideMenu() {
 
           
            
-              <li onClick={()=>setActiveEnseignant(!isActiveEnseignant)}>
+              <li onClick={()=>setActiveEnseignant(!isActiveEnseignant)} className="unselectable ">
                   <SideMenuItems source={require("../../../res/images/teacher2.png")} alt={""} text={"Enseignant"}/>
               </li>
+              {/*make an animation to the list items :D */}
               {isActiveEnseignant ? 
+                <>
                 <ul className="sub-text">
-                <Link to="/adminDashBorad/AllEnseignant"><li><FontAwesomeIcon icon={faUsers} className="font-space"/>tous les Enseignant</li></Link>
-                <Link to="/adminDashBorad/enseignantValid"><li><FontAwesomeIcon icon={faUserCheck} className="font-space"/> Enseignant valide</li></Link>
-                <Link to="/adminDashBorad/enseignantNotValid"> <li><FontAwesomeIcon icon={faUserXmark} className="font-space"/>Enseignant non valide</li></Link>
+                <motion.div
+                initial={{ opacity:0 }}
+                animate={{ opacity:1 }}
+                transition={{ duration: 1 }}
+              >
+              
+              <Link to="/adminDashBorad/AllEnseignant"><li><FontAwesomeIcon icon={faUsers} className="font-space"/>tous les Enseignant</li></Link>
+              </motion.div>
+              <motion.div
+                initial={{ opacity:0 }}
+                animate={{ opacity:1 }}
+                transition={{ duration: 2 }}
+              >
+              <Link to="/adminDashBorad/enseignantValid"><li><FontAwesomeIcon icon={faUserCheck} className="font-space"/> Enseignant valide</li></Link>
+              </motion.div>
+              <motion.div
+                initial={{ opacity:0 }}
+                animate={{ opacity:1 }}
+                transition={{ duration: 3 }}
+              >
+              <Link to="/adminDashBorad/enseignantNotValid"> <li><FontAwesomeIcon icon={faUserXmark} className="font-space"/>Enseignant non valide</li></Link>
+              </motion.div>
+              
+              </ul>
+               
+               </>:null}
+                
+                
                   
-                </ul>:null}
+                
               
          
             
             
-              <li onClick={()=>setActiveEtudiant(!isActiveEtudiant)}>
+              <li onClick={()=>setActiveEtudiant(!isActiveEtudiant)} className="unselectable ">
                 <SideMenuItems source={require("../../../res/images/reading-book (1).png")} alt={""} text={"Etudiant"}/>
               </li>
               {isActiveEtudiant ? 
+                <>
                 <ul className="sub-text">
+                
+                
+                <motion.div
+                initial={{ opacity:0 }}
+                animate={{ opacity:1 }}
+                transition={{ duration: 1 }}
+                >
                 <Link to="/adminDashBorad/AllEtudiant"><li><FontAwesomeIcon icon={faUsers} className="font-space"/>tous les Etudiants</li></Link>
-                <Link to="/adminDashBorad/etudiantValid"><li><FontAwesomeIcon icon={faUserCheck} className="font-space"/> Etudiants valide</li></Link>
-                <Link to="/adminDashBorad/etudiantNotValid"><li><FontAwesomeIcon icon={faUserXmark} className="font-space"/>Etudiants non valide</li></Link>
+              </motion.div>
+              <motion.div
+              initial={{ opacity:0 }}
+              animate={{ opacity:1 }}
+              transition={{ duration: 2 }}
+              >
+              <Link to="/adminDashBorad/etudiantValid"><li><FontAwesomeIcon icon={faUserCheck} className="font-space"/> Etudiants valide</li></Link>
+              </motion.div>
+              <motion.div
+              initial={{ opacity:0 }}
+              animate={{ opacity:1 }}
+              transition={{ duration: 3 }}
+              >
+              <Link to="/adminDashBorad/etudiantNotValid"><li><FontAwesomeIcon icon={faUserXmark} className="font-space"/>Etudiants non valide</li></Link>
+              </motion.div>
+              </ul>
                   
-                </ul>:null}
+                </>:null}
               
             
 
